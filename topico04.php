470ce6b65cae06b4f8a76b732e47e4e993d22622
julia <mariajuliaamaraldasilva@gmail.com>
2023-10-20 16:58:13 -0300

    terminando o tópico 04

--- topico04.php
+++ topico04.php
@@ -81,7 +81,7 @@ A inspiração nesse projeto é válida, mas é preciso delimitar o alcance do s
 <p class="text-left">Note que algumas tipologias são sociais ou culturais. São categorias não relacionadas diretamente com a educação, mas que podem fazer parte do evento em momentos específicos através de intervenções artísticas.</p>
 
 <p class="text-left">Resumimos e esclarecemos algumas tipologias com base em (Zitta, 2014):</p>
-
+<div class="text-left">
 <!-- TABELA DO TÓPICO 04 -->
 <table class="table table-bordered  table-striped" id="cor-table">
         <thead>
@@ -93,7 +93,6 @@ A inspiração nesse projeto é válida, mas é preciso delimitar o alcance do s
                                         <tbody>
                                             <tr class="tBody">
                                                 <td data-th="Tipo"><em>Debate</em></td>
-                                                <td data-th="Detalhamento"><em>Palestrante não consegue se comunicar com a equipe ao vivo;</em></td>
                                                 <td data-th="Ações">
                                                 O debate acontece quando o tema sucinta posições contrárias ou mesmo quando fortes dúvidas formam blocos de opiniões divergentes.
                                                 Ele pode acontecer no decorrer de uma discussão, quando o conceito, uma apreciação ou tese não alcança unanimidade. Neste caso, os elementos se voltam para defender os próprios pontos de vista.
@@ -105,13 +104,204 @@ A inspiração nesse projeto é válida, mas é preciso delimitar o alcance do s
                                             <tr class="tBody">
                                                 <td data-th=""><em>Mesa-redonda</em></td>
                                                 <td data-th="Plataforma de Captação"><em>É a reunião de especialistas, sustentando posições divergente a respeito de um tema. Grupo de pessoas especializadas em determinado assunto aprenda e expõe pontos de vista divergentes ou contraditórios sob o mesmo tema.
-Visa dar ao auditório a oportunidade de conhecer o pensamento de técnicos ou especialistas sobre um assunto controvertido.</em></td>
-                                                
+                                                Visa dar ao auditório a oportunidade de conhecer o pensamento de técnicos ou especialistas sobre um assunto controvertido.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Fórum</em></td>
+                                                <td data-th="Plataforma de Captação"><em>	Entende-se como uma reunião grupo da qual todos os presentes em uma sala, reunião ou grupo tem a oportunidade de participar ativamente. É organizado com a finalidade de debater um tema ou problema.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Simpósio</em></td>
+                                                <td data-th="Plataforma de Captação"><em>É o método onde duas ou mais pessoas, versadas em determinado assunto, sob a direção de um coordenador, expõe um tema sobre sua especialidade, cada qual apresentando uma parte ou focalizando seus pontos de vista diferentes, contando com a participação do auditório no final da sessão por meio de perguntas.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Seminário</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Sua finalidade é a investigação ou estudo de um tema em reuniões de trabalho devidamente planejados. É um grupo de trabalho ativo, pois os membros não recebem às informações elaboradas. Pesquisam por seus próprios meios em colaboração recíproca.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Painel</em></td>
+                                                <td data-th="Plataforma de Captação"><em>
+                                                É uma reunião de pessoas especializadas expondo ideias sobre um determinado assunto ao auditório de maneira informal e dialogada em troca de ideias, expondo e apreciando diferentes perspectivas.
+                                                </em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Brainstorming</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Um grupo se reúne em assembleia quando tem por finalidade encontrar ideias ou soluções novas, sendo necessário o uso da imaginação. O objetivo é produzir o maior número de ideias sobre um tema ou problema.</em></td>
+                                            </tr>
+
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Entrevista Coletiva</em></td>
+                                                <td data-th="Plataforma de Captação"><em>
+                                                Evento em que autoridade de uma instituição, ou seu representante, comunica à imprensa ou aos seus profissionais um fato novo de alta relevância, abrindo, em seguida, espaço para questionamentos.
+                                                </em></td>
+                                            </tr>
+
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Exposição</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Evento em que há um “cenário” cuidadosamente preparado para apresentação de resultados de uma empresa ou instituição e atender aos questionamento do público visitante. Podem ser agropecuárias, industriais, comerciais, informática etc.</em></td>
+                                            </tr>
+
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Mostra</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Evento onde a empresa apresenta resultados de trabalhos, documentos históricos ou material artístico/cultural produzido por colaboradores ou convidados.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Lançamento</em></td>
+                                                <td data-th="Plataforma de Captação"><em>
+                                                Evento utilizado para apresentar novas tecnologias, serviços e produtos ou novidades que tragam contribuições significativas à sociedade.
+                                                </em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Pedra Fundamental</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Evento que marca o início de uma obra.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Palestra</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Evento caracterizado pela apresentação de um tema, por um ou mais especialistas, a um grupo de pessoas com interesses comuns.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Curso</em></td>
+                                                <td data-th="Plataforma de Captação"><em>É a capacitação de pessoas para execução de tarefas específicas. O objetivo do evento é mais teórico do que prático, porém não invalida a prática. É indicado para pessoas que têm baixo ou nenhum conhecimento sobre o assunto. Com exceção dos cursos de especialização, que têm o objetivo de aperfeiçoar o conhecimento de quem já domina o assunto.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Workshop</em></td>
+                                                <td data-th="Plataforma de Captação"><em>É o termo em inglês para definir oficina de trabalho. É um evento dividido em: teoria e prática. Os participantes devem ser da mesma área ou segmento de trabalho e se reúnem para debater, praticar e procurar soluções para o tema proposto.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Congresso</em></td>
+                                                <td data-th="Plataforma de Captação"><em>
+                                                É uma reunião formal e periódica de profissionais atuando na mesma área, promovida por entidades associativas e inclui debates e diversas outras tipologias.
+                                                </em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Encontro</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Evento onde pessoas da mesma categoria profissional e interesses comuns debates temas polêmicos. Pode ser estadual, regional, nacional ou internacional.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Jornada</em></td>
+                                                <td data-th="Plataforma de Captação"><em>
+                                                São encontros de grupos profissionais mais próximos, de âmbito regional, que discutem, periodicamente, assuntos de interesse do grupo. As conclusões podem definir linhas norteadoras para os trabalhos em grupo e para as profissões. Jornadas podem durar vários dias, isso depende da complexidade e do tamanho do evento.
+                                                </em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Feira</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Evento onde, em um estande, a empresa apresenta os resultados de seu trabalho e comercializa suas tecnologias, produtos e serviços.</em></td>
+                                            </tr>
+
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Rodada de Negócios</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Evento onde se reúnem empresários de um determinado segmento, devidamente inscritos, para vender ou comprar produtos e serviços.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Convenção</em></td>
+                                                <td data-th="Plataforma de Captação"><em>
+                                                Reunião de pessoas de departamentos ou seções de uma empresa objetivando: conhecer novas diretrizes, treinamento, reciclagem, entrosamento, troca de experiências e informações.
+                                                </em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Semana</em></td>
+                                                <td data-th="Plataforma de Captação"><em>
+                                                Pode ser de dois tipos: acadêmica (estudantes coordenados por professores e apoiados por profissionais da área do mesmo segmento) ou empresarial (reunião de pequenos eventos empresariais tematizados).
+                                                </em></td>
+                                            </tr>
+
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Conferência</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Caracteriza-se pela apresentação de um tema informativo geralmente técnico ou científico por uma autoridade em determinado assunto para um grande número de pessoas.</em></td>
+                                            </tr>
+
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Colóquio</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Semelhante à conferência, apresentado por conferencista de renome e expert no assunto. Possui objetivo de esclarecimento de um tema ou tomada de decisão. Utilizado mais em classes específicas, por exemplo, classe médica.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Reuniões</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Reúne grupos de pessoas na empresa ou de outras instituições para discutir temas relacionados com suas atividades administrativas e políticas.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Posse</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Realizado em auditório sentado ou em salão em pé.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Salão</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Até algum tempo atrás o conceito de salão era semelhante ao de feira, mas menos amplo, sem objetivo de venda ou mercadológicos. Atualmente é considerado um sinônimo de feira e pode ser realizado por empresas diferentes no mesmo espaço e segmento de mercado.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Inauguração</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Apresentação da empresa ou instituição ao público-alvo, entidade ou governo. Envolve um cerimonial específico.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Torneio</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Possui caráter esportivo e competitivo. Geralmente é mais completo, envolve várias etapas até a definição de quem será o vencedor.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Gincana</em></td>
+                                                <td data-th="Plataforma de Captação"><em>	Competição organizada por uma comissão que define as tarefas a serem realizadas pelas equipes e regras para definição de vendedores.</em></td>
+                                            </tr>
+
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Show</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Encontro artístico com foco em música, caracterizado pela participação do público-alvo em um evento essencialmente de entretenimento.</em></td>
+                                            </tr>
+
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Talk-show</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Evento que associa uma entrevista pessoal com artista ou profissional de renome acompanhado por atração musical.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Sarau / Lual</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Evento composto por amigos, textos, músicas em um local descontraído com petiscos.</em></td>
+                                            </tr>
+
+                                            <tr class="tBody">
+                                                <td data-th=""><em>Vernissage</em></td>
+                                                <td data-th="Plataforma de Captação"><em>Evento demonstrativo caracterizado pelo lançamento conceitual e comercial de peças, itens ou coleções artísticas, individuais ou coletivas.</em></td>
                                             </tr>
                                         </tbody>
                                     </table>
+                         <!-- FIM DA TABELA  -->
+                         </div>
 
-<!-- FIM DA TABELA  -->
+                         <div class="text-left">
+                            <p>Muita coisa, não é mesmo? Ainda bem que não é preciso ter na ponta da língua todos os conceitos, afinal, esse resumo foi feito para você consultar as tipologias durante a sua reunião de criação de evento.</p>
+
+                            <p>Encerramos aqui o curso de Eventos Digitais como Ferramenta de Educação. Gostou do conteúdo?</p>
+
+                            <p><strong>Agora chegou a hora de colocar em prática esses conhecime ntos e criar o seu primeiro evento digital!</strong></p>
+
+                            <p><strong>Para concluir o curso, vá para a avaliação.</strong></p>
+                         </div>
 </div>
 </div>
 
