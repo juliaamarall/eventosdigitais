f24194dfb66ea4157d28c5a879c76617d561173e
julia <mariajuliaamaraldasilva@gmail.com>
2023-11-07 13:00:08 -0300

    Organizando os scripts e cdns no Header

--- layout/header.php
+++ layout/header.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- cdn scrollreveal -->
+    <script src="https://unpkg.com/scrollreveal"></script>
     <!-- CDN DO BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
@@ -12,16 +14,14 @@
         crossorigin="anonymous"></script>
     <!-- Cdn do bootstrap icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <!-- link do font awesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- nosso css -->
     <link rel="stylesheet" href="css/style.css">
     <!-- css do curso  -->
     <link rel="stylesheet" href="css02/styles.css">
-    <!-- link do font awesome  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- link do script -->
     <script src="js/script.js" defer></script>
-    <!-- cdn scrollreveal -->
-    <script src="https://unpkg.com/scrollreveal"></script>
     <!-- Favicon -->
     <link rel="shortcut icon" href="http://ava.egp.ce.gov.br/2018/desv/logo/favicon.png" />
 
